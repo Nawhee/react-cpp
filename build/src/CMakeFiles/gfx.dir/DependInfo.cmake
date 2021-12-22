2ef3fa39acf11c8360cc9b28551e43c3116b39c6
Nahiyan Hussain <nahiyan.hussain@gmail.com>
2021-12-22 00:12:35 -0800

    workig

---
+++ build/src/CMakeFiles/gfx.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/nahiy/Desktop/nightmarket/c++/3D Abstraction/Engine/src/gfx/CreateWindow.cpp" "src/CMakeFiles/gfx.dir/gfx/CreateWindow.cpp.obj" "gcc" "src/CMakeFiles/gfx.dir/gfx/CreateWindow.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
